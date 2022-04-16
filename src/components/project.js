7a038e7441377dce0ddee0b6c3aa0ff9c18a3578
Jack <jackowendickson@gmail.com>
2022-04-16 14:38:16 +0100

    project titles are listing correctly

--- src/components/project.js
+++ src/components/project.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Project = () => {
+const Project = ({project}) => {
 
     return (
-        <p>A project</p>
+        <h4>{project.title}</h4>
     )
 }
 
--- src/containers/ProjectBox.js
+++ src/containers/ProjectBox.js
@@ -4,12 +4,25 @@ import Project from "../components/Project"
 
 const ProjectBox = () => {
 
+    const featuredProjects = [
+        {title: "Ghibli Gallery",
+        description: "A web application that grabs data from a Studio Ghibli api and displays information about the films. Films can be seleceted for more information",
+        madewith: ["JavaScript", "React"]},
+        {title: "Hobbyist",
+        description: "An application than manages members, lessons and bookings through a web page",
+        madeWith: ["python3", "postgreSQL", "flask"]}
+    ]
 
+    const projectNodes = featuredProjects.map( (project, index) => {
+        return (
+            <Project project={project}/>
+        )
+    })
 
     return (
     <div>
         <h3>Some of my Projects</h3>
-        <Project/>
+        {projectNodes}
     </div>
     )
 }
