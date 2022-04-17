da68c2d422a630204da14ee0ca13f4977d76a74e
Jack <jackowendickson@gmail.com>
2022-04-17 16:08:19 +0100

    added animations, icons which link to my github and linked in and changed spacing around the about me box

--- src/containers/AboutMe.js
+++ src/containers/AboutMe.js
@@ -1,17 +1,59 @@
 import React from "react";
 import styled from "styled-components"
+import { keyframes } from "styled-components";
+import whiteGithub from "../img/white-github-60.png"
+import whiteLinkedin from "../img/white-linkedin-60.png"
 
 const AboutMe = () => {
 
+    //styled
+
     const StyledAboutMe = styled.div`
         color: white;
+        text-align: center;
         margin: 0%;
-        padding: 100px;`
+        padding: 75px;
+        padding-bottom: 0px;
+        `
+
+    const waveRight = keyframes`
+    from {
+        transform: rotate(0deg);
+    }
+    to  {
+        transform: rotate(100deg);
+    }`
+
+    const Wave = styled.h1`
+        animation: ${waveRight} 2s ease-in-out 0.5s infinite alternate;
+    `
+
+    const ExternalLinks = styled.img`
+    width: 45px;
+    height: 45px;
+    margin-bottom: 30px;
+    &:hover {
+            width: 60px;
+            height: 60px;
+            background: #3271a8;
+            border-radius: 30px;
+            margin-bottom: 15px;
+        }
+    `
 
     return (
     <StyledAboutMe>
-        <h2>Hello I am Jack</h2>
+        <h1>Hello! My name is Jack!</h1>
+        <Wave>👋🏻</Wave>
         <p>I am currently learning how to program</p>
+        <ul>
+            <a href="https://github.com/JackOfDickson">
+            <ExternalLinks src={whiteGithub}></ExternalLinks>
+            </a>
+            <a href="https://www.linkedin.com/in/jackdickson/">
+            <ExternalLinks src={whiteLinkedin}></ExternalLinks>
+            </a>
+        </ul>
 
     </StyledAboutMe>
     )
--- src/pages/Page.js
+++ src/pages/Page.js
@@ -10,7 +10,7 @@ const Page = () => {
     const StyledPage = styled.div`
     height: 100vh;
     background: rgb(2,0,31);
-    background: linear-gradient(180deg, rgba(2,0,31,1) 0%, rgba(0,0,101,1) 60%, rgba(0,120,209,1) 100%);
+    background: linear-gradient(180deg, rgba(2,0,31,1) 0%, rgba(0,0,101,1) 20%, rgba(0,120,209,1) 100%);
 
     `
 
